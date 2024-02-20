693890723cc264077f721e9bf2a8ecc078fbacbb
MarioAntonioOC <oiram90140@gmail.com>
2024-02-19 22:22:31 -0600

    Primer commit

---
+++ src/test/java/com/example/zonaCity/ZonaCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zonaCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonaCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
